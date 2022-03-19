5901fd8c7cc43221cc887bbf21ca9a80e23d3872
enomilan <andrewmusta@gmail.com>
2022-03-18 20:18:13 -0500

    start

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         {
           "headers": {
           "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-          "x-rapidapi-key": 'xx' }
+          "x-rapidapi-key": "ffef827568msh1decf807055fb86p1b5c15jsndfd56fb80151" }
         })
         const player = await res.json()
                                   
@@ -35,7 +35,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          
+          <input type ='text'></input>
         </p>
         
       </header>
